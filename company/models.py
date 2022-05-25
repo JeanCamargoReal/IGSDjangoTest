828c4eeac4a1d36212daf5b6269d8c214cd2d6a1
JeanCamargoReal <jeancamargoreal@icloud.com>
2022-05-25 20:52:39 -0300

    Adiciona model do Employee

--- company/models.py
+++ company/models.py
@@ -1,3 +1,14 @@
+import uuid
+
 from django.db import models
 
+
 # Create your models here.
+class Employee(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=150, blank=False, null=False)
+    email = models.EmailField(max_length=150, blank=False, null=False)
+    departament = models.CharField(max_length=150, blank=True, null=False)
+    
+    def __str__(self):
+        return self.name
