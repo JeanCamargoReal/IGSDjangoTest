615887165e214193504e133aa76b4cb990ae7d42
JeanCamargoReal <jeancamargoreal@icloud.com>
2022-05-25 21:20:18 -0300

    Adiciona view de Employee

--- company/views.py
+++ company/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from urllib import response
 
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import Employee
+from .serializers import EmployeeSerializer
+
+
+class EmployeeAPIView(APIView):
+    def get(self, request):
+        employees = Employee.objects.all()
+        serializer = EmployeeSerializer(employees, many=True)
+        
+        return Response(serializer.data)
