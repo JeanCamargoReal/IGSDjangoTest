a77a7c602ca017907f6a40ef45bf3899b98414b3
JeanCamargoReal <jeancamargoreal@icloud.com>
2022-05-25 22:47:44 -0300

    Refatora model

--- company/models.py
+++ company/models.py
@@ -5,7 +5,6 @@ from django.db import models
 
 # Create your models here.
 class Employee(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=150, blank=False, null=False)
     email = models.EmailField(max_length=150, blank=False, null=False)
     departament = models.CharField(max_length=150, blank=True, null=False)
